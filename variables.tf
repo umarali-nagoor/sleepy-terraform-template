cf620180ce1e5a282b69ca4fc7316710c70b140a
AnilCM <amallak1@in.ibm.com>
2021-06-30 08:56:02 +0530

    changed flow

--- variables.tf
+++ variables.tf
@@ -2,5 +2,16 @@ variable "subnet_name"{
   default = "testsubnet"
 }
 
+variable "ssh_key_id" {
+  default = "r006-adb7393f-1a5b-4671-8dbb-f0dcc6810b63"
+}
+
 variable "ibmcloud_api_key"{
+}
+
+variable "image" {
+  default = "r006-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
+}
+variable "profile" {
+  default = "cx2-2x4"
 }
