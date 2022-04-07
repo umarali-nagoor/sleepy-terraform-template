9bba8d3b2db0fa8865162bde28b757723d439cde
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2022-04-07 13:48:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,12 @@
-variable "subnet_name"{
+variable "vpc_name"{
   default = "testsubnet"
 }
 
-variable "ssh_key_id" {
-  default = "r006-687f4e20-6904-4f66-90c2-6c4d92c16262"
+variable "resource_group" {
+  type = string
 }
 
-variable "ibmcloud_api_key"{
+variable "vpc_tags" {
+  type = list(string)
 }
 
-variable "image" {
-  default = "r006-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
-}
-variable "profile" {
-  default = "cx2-2x4"
-}
