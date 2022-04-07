8324f25d0e9b3fc23e87cd05465029beea1de7e5
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2022-04-07 19:18:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
-data "ibm_resource_group" "group" {
-  name = var.resource_group
-}
+
 
 resource "ibm_is_vpc" "vpc" {
   name = var.vpc_name
-  resource_group = data.ibm_resource_group.group.id
   tags = var.vpc_tags
 }
