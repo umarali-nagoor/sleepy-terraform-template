390430ff1aa45ae6c641bc1fa1fa3f685b66446d
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2022-04-07 13:46:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,5 @@
 resource "ibm_is_vpc" "vpc" {
-  name = "driftvpc"
-}
-resource "ibm_is_subnet" "subnet" {
-  name            = var.subnet_name
-  vpc             = ibm_is_vpc.vpc.id
-  zone            = "us-south-1"
-  total_ipv4_address_count  = 256
-}
-
-resource "ibm_is_instance" "instance1" {
-  name     = "automation-instance"
-  image    = var.image
-  profile  = var.profile
-  primary_network_interface {
-    subnet = ibm_is_subnet.subnet.id
-  }
-  vpc       = ibm_is_vpc.vpc.id
-  zone      = "us-south-1"
-  keys      = [var.ssh_key_id]
-}
-resource "ibm_is_security_group" "sg" {
-  name = "test-sg"
-  vpc  = ibm_is_vpc.vpc.id
-}
-resource "ibm_is_security_group_rule" "outbound" {
-  group     = ibm_is_security_group.sg.id
-  direction = "outbound"
-  remote = "127.0.0.1"
-}
-resource "ibm_is_security_group_rule" "inbound" {
-  group     = ibm_is_security_group.sg.id
-  direction = "inbound"
-  remote = "127.0.0.1"
-}
-resource "ibm_is_security_group_rule" "icmp" {
-  group     = ibm_is_security_group.sg.id
-  direction = "inbound"
-  remote    = "127.0.0.1"
-  icmp {
-    code = 20
-    type = 30
-  }
+  name = var.vpc_name
+  resource_group = var.resource_group
+  tags = var.vpc_tags
 }
