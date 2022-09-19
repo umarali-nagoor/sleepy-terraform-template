3dc60ea7fb30f236dbadfa817cf4beb5d337808d
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2022-09-19 23:29:29 +0530

    Update my_env.tf

--- my_env.tf
+++ my_env.tf
@@ -1,5 +1,5 @@
-resource "null_resource" "ibmcloud-login" {
-    provisioner "local-exec" {
-        command = "env"
-    }
-}
+#resource "null_resource" "ibmcloud-login" {
+#    provisioner "local-exec" {
+#        command = "env"
+#    }
+#}
