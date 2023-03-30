ce1d787f6da94daffdc815b58eae8d723c68698e
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2023-03-30 16:26:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
 
 
-resource "ibm_is_vpc" "vpc" {
-  name = var.vpc_name
-  tags = var.vpc_tags
+# resource "ibm_is_vpc" "vpc" {
+#   name = var.vpc_name
+#   tags = var.vpc_tags
+# }
+# This resource will destroy (potentially immediately) after null_resource.next
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "30s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_30_seconds]
 }
