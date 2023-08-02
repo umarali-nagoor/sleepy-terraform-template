1bc92d2d89959727abe0af7e583961762fda5bb1
Umar Ali <umarali@Umars-MacBook-Pro.local>
2023-08-02 12:59:56 +0530

    Updated template 2

--- main.tf
+++ main.tf
@@ -1,23 +1,4 @@
-
-
-# resource "ibm_is_vpc" "vpc" {
-#   name = var.vpc_name
-#   tags = var.vpc_tags
-# }
-# This resource will destroy (potentially immediately) after null_resource.next
-resource "null_resource" "previous" {}
-
-resource "time_sleep" "wait_300_seconds" {
-  depends_on = [null_resource.previous]
-
-  create_duration = "300s"
+# Create an arbitrary local resource
+data "template_file" "test" {
+  template = "Hello ${var.name}!"
 }
-
-# This resource will create (at least) 300 seconds after null_resource.previous
-resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_300_seconds]
-}
-
-#data "ibm_is_vpc" "example" {
-#  name = var.vpc_name
-#}
---
+++ vars.tf
@@ -0,0 +1,6 @@
+# Configure these variables
+
+variable "name" {
+  description = "The name to pass to the template."
+  default     = "I Am xecuted on an agent"
+}
